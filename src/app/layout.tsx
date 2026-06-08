849b33bdbbbf57711e86b22a385482674ba0e15e
Riski_ <riskiafrizal11@gmail.com>
2026-06-08 21:13:23 +0700

    style: import surat jalan print styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import "./phase10c.css";
 import "./phase10d.css";
 import "./phase10e.css";
 import "./phase12.css";
+import "./surat-jalan.css";
 import "../styles/dashboard.css";
 import { Toaster } from "sonner";
 
