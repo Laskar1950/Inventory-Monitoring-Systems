71f3172d673cdeaca062f29b92b353d8bf3c5c01
Riski_ <riskiafrizal11@gmail.com>
2026-06-11 09:35:46 +0700

    fix: load modal centering overrides last

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ import "./phase10e.css";
 import "./phase12.css";
 import "./phase14a.css";
 import "./surat-jalan.css";
-import "./modal-fixes.css";
 import "../styles/dashboard.css";
+import "./modal-fixes.css";
 import { Toaster } from "sonner";
 
 const inter = Inter({
